a2f3ee617078eb92d1c3dde885da81bde802ef92
Fedi Nabli <fedinabli@gmail.com>
2022-10-01 00:37:24 +0100

    Adde Hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Fedi's Portfolio</title>
       </Head>
@@ -12,6 +13,9 @@ const Home: NextPage = () => {
       <Header />
 
       {/* Hero */}
+      <section id='hero' className='snap-center'>
+        <Hero />
+      </section>
 
       {/* About */}
 
