e04154daefe7e5d13c50e5d54dd72cea7ac62fcb
Fedi Nabli <fedinabli@gmail.com>
2022-10-02 13:18:33 +0100

    Fixed background circles padding

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -19,11 +19,11 @@ function BackgroundCircles({}: Props) {
       }}
       className='relative flex justify-center items-center'
     >
-      <div className='absolute border border-[#333333] rounded-full w-[200px] h-[200px] mt-96 animate-ping' />
-      <div className='absolute border border-[#333333] rounded-full w-[300px] h-[300px] mt-96' />
-      <div className='absolute border border-[#333333] rounded-full w-[500px] h-[500px] mt-96' />
-      <div className='absolute border border-[#F7AB0A] rounded-full opacity-20 w-[650px] h-[650px] mt-96 animate-pulse' />
-      <div className='absolute border border-[#333333] rounded-full w-[800px] h-[800px] mt-96' />
+      <div className='absolute border border-[#333333] rounded-full w-[200px] h-[200px] mt-52 animate-ping' />
+      <div className='absolute border border-[#333333] rounded-full w-[300px] h-[300px] mt-52' />
+      <div className='absolute border border-[#333333] rounded-full w-[500px] h-[500px] mt-52' />
+      <div className='absolute border border-[#F7AB0A] rounded-full opacity-20 w-[650px] h-[650px] mt-52 animate-pulse' />
+      <div className='absolute border border-[#333333] rounded-full w-[800px] h-[800px] mt-52' />
     </motion.div>
   )
 }
